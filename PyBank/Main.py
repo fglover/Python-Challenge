f02c2cfc8c300db1bc8c4dc4d4d8f57ce9884af7
fglover <fgloverucfbc@gmail.com>
2019-02-09 11:43:38 -0500

    Update Main.py

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -5,7 +5,7 @@ import os
 # Module for reading csv file
 import csv
 
-csvpath = os.path.join('budget_data.csv')
+csvpath = os.path.join('..','Resources','budget_data.csv')
 
  #Reading using csv module
 with open(csvpath, newline='') as csvfile:
